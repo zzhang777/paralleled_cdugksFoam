387366f377d097980d07f8ad280a31d7f2e660b6
zhangqi <izhangqii@163.com>
2022-02-18 09:14:59 +0800

    add decompose script

---
+++ multidecompose.py
@@ -0,0 +1,71 @@
+import os
+import re
+import sys
+import argparse
+from shutil import copyfile
+
+
+def copy_file(path_read, path_write, i):
+    names = os.listdir(path_read)
+    for name in names:
+        path_read_new = os.path.join(path_read, name)
+        path_write_new = os.path.join(path_write, name)
+        if os.path.isdir(path_read_new):
+            if not os.path.exists(path_write_new):
+                os.makedirs(path_write_new)
+            copy_file(path_read_new, path_write_new, i)
+        else:
+            copyfile(path_read_new, path_write_new)
+            if('0' in path_write_new or name == 'boundary'):
+                modify_file(path_write_new, i)
+
+
+def modify_file(file_name, i):
+    shift = phy_num*(i//phy_num)
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+    with open(file_name, "w") as f_w:
+        for line in lines:
+            if "procBoundary" in line or "myProcNo" in line or "neighbProcNo"in line:
+                line = re.sub('\d+', lambda x: str(int(x.group())+shift), line)
+	    if "numberOfSubdomains" in line:
+                line = re.sub('\d+',str(i),line)
+            f_w.write(line)
+
+
+   
+
+if __name__ == "__main__":
+    phy_num = 1
+    vel_num = 1
+    root = os.getcwd()
+    dict_root=os.path.join(root,'system','decomposeParDict')
+
+#need rm -f to remove the exist processorx floder
+    os.system("rm -r processor*")
+
+    parser = argparse.ArgumentParser(description='decompose physics space used for phy&vel parallel.')
+    parser.add_argument('-p', required=True, help="the number of physics domain one wish to decompose", type=int)
+    parser.add_argument('-v', required=True, help="the number of velocity domain one wish to decompose", type=int)
+    args = parser.parse_args()
+    phy_num = args.p
+    vel_num = args.v
+   
+    #modify numofsubdomains in decomposePar
+    modify_file(dict_root,phy_num)
+    os.system("decomposePar")
+    for i in range(phy_num):
+        dir='processor'+str(i)
+        os.system(" cp -r constant/vMesh/     %s/constant/"  % (dir))
+    
+    for i in range(phy_num, phy_num * vel_num):
+        path_read = os.path.join(root, 'processor'+str(i % phy_num))
+        path_write = os.path.join(root, 'processor'+str(i))
+        copy_file(path_read, path_write, i)
+    modify_file(dict_root,phy_num*vel_num)
+    
+	#copy vMesh
+    os.system("echo ./processor*/constant | xargs -n 1 cp -r ./constant/vMesh")
+
+	
+
